9f626dfa63dbc5b0ba3fb560dc1df569f3710eaf
Termineitor320 <danieldiaz@colegioguanenta.edu.co>
2025-07-31 16:03:09 -0500

    se agrego ventana principal

---
+++ ventana_1.py
@@ -0,0 +1,22 @@
+# se importala libreria tkinter 
+from tkinter import *
+# -----------------------------
+# funciones de la app 
+# -----------------------------
+
+# -----------------------------
+# ve ntana principal de la app
+# -----------------------------
+
+# se declara una variable  llamada ventana_principal, que anquiere las caracteristicas de un juego
+
+ventana_principal= Tk()
+
+# Titulo de ventana 
+ventana_principal.title("Daniel Diaz ")
+
+# Tamaño de la ventana 
+ventana_principal.geometry("800x500")
+
+# metodo principal que despliega la ventana en pantalla 
+ventana_principal.mainloop()
